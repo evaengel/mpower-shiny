e539c13a6f65231adcb0d5ab81f5b18c80098bea
Eva Engel <70496031+evaengel@users.noreply.github.com>
2022-09-18 22:41:10 -0400

    interpolation code

---
+++ interpolation-code/BWS Interpolation Code.R
@@ -0,0 +1,19 @@
+#read in bws simulation results
+modelsa <- read.csv("C:\\Users\\eva\\Documents\\Mpower - Duke Project\\dataz.csv")
+modelsb <- as.data.frame(modelsa)
+#select useful columns in dataframe
+modelsc <- modelsb[,2:6]
+
+##Interpolation for bws simulations
+#select sample sizes
+samplesize <- c(modelsc$Sample.Size,2000)
+#select power
+pow <- c(modelsc$power,1)
+#interpolate for power
+z <- approx(samplesize, pow, xout=20:2000, method= 'linear')
+#store interpolation results as data frame
+zdataframe <- data.frame(x = z$x, y = z$y)
+#labels z$x column as Sample_Size and z$y column as Power
+colnames(zdataframe) <- c("Sample_Size","Power")
+#write results into csv
+write.csv(zdataframe,"bws-simulation-data.csv",row.names = TRUE)
