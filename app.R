f9132b93e96b4b0d8534bf52de6884a21359e3cb
Eva Engel <ee2591@princeton.edu>
2024-02-03 14:38:48 -0500

    Add BKMR and effect sizes to Shiny app

--- app.R
+++ app.R
@@ -1,14 +1,24 @@
 ##CSV description
-#data/glm-simulation-data.csv contains simulation results for GLM models
+#glm-interpolation-results-large.csv contains simulation results for GLM models 
+        #with large effect size
+#glm-interpolation-results-small.csv contains simulation results for GLM models 
+        #with small effect size
+#bws-interpolation-results-large.csv contains simulation results for BWS models
+        #with large effect size
+#bws-interpolation-results-small.csv contains simulation results for BWS models
+        #with small effect size
+#bkmr-interpolation-results-large.csv contains simulation results for BKMR models
+        #with large effect size
+#bkmr-interpolation-results-small.csv contains simulation results for BKMR models
+        #with small effect size
 #data/nhanes-data.csv contains NHANES data used to compute correlation matrix
-#data/bws-simulation-data.csv contains simulation results for BWS models
 
 ##app description
-#contains bws & glm model simulation results (you can select for sample size and 
-        #what model to view)
+#contains bws & glm & bkmr model simulation results
 #allows user to select significance threshold
 #allows user to select group of chemicals
 #allows user to select effect
+#allows user to select effect level
 #displays true outcome model
 #highlights cells with power > 0.8
 #contains correlation matrix for NHANES variables UrinaryBisphenolA, 
@@ -31,7 +41,7 @@ ui <- fluidPage(
         sidebarLayout(
                 sidebarPanel(
                         #Input select (select glm or bws model to display)
-                        selectInput(inputId = "sct", label = "Select Model", c("GLM","BWS")),
+                        selectInput(inputId = "sct", label = "Select Model", c("GLM","BWS","BKMR")),
                         #Input slider for sample size (goes in increments of 1)
                         sliderInput(inputId = "num", label = "Select a Sample Size", 
                                     value=20, min=20, max=2000, step = 1),
@@ -40,7 +50,8 @@ ui <- fluidPage(
                         selectInput(inputId = "group", label = "Select a Group of Chemicals",
                                     c("Consumer products chemicals")),
                         selectInput(inputId = "effect", label = "Select an Effect",
-                                    c("Linear, additive"))
+                                    c("Linear, additive")),
+                        selectInput(inputId = "effectsize", label = "Select an Effect Size", c("Small","Large"))
                 ),
                 #Output() function (creates datatable and plot as output)
                 mainPanel(
@@ -56,42 +67,101 @@ ui <- fluidPage(
                 )
         )
 )
-#reads in csv for glm simulation results
-models <- read.csv("data/glm-simulation-data.csv")
+#reads in csv for glm interpolation results for large effect size
+models <- read.csv("glm-interpolation-results/glm-interpolation-results-large.csv")
 #turns results into dataframe
 models2 <- as.data.frame(models)
 #selects useful columns in dataframe (variable, sample size, power)
-models3 <- models2[,2:4] %>%
+glm_large <- models2[,2:4] %>%
         mutate_if(is.numeric, round, 2)
 #renames columns in dataframe
-colnames(models3) <- c("Chemical","Sample Size","Power")
+colnames(glm_large) <- c("Chemical","Sample Size","Power")
 #read in nhanes1 csv (for correlation matrix)
 nhanes1 <- read.csv("data/nhanes-data.csv")
 #remove X column from nhanes1
 nhanes2 <- nhanes1[,c(2:10)]
 
-#reads in csv for bws simulation results
-modelsa <- read.csv("data/bws-simulation-data.csv") # used to be results of bws interpolation, need to change for small effect sizes
+#reads in csv for glm interpolation results for small effect size
+models <- read.csv("glm-interpolation-results/glm-interpolation-results-small.csv")
+#turns results into dataframe
+models2 <- as.data.frame(models)
+#selects useful columns in dataframe (variable, sample size, power)
+glm_small <- models2[,2:4] %>%
+        mutate_if(is.numeric, round, 2)
+#renames columns in dataframe
+colnames(glm_small) <- c("Chemical","Sample Size","Power")
+
+
+#reads in csv for bws simulation results for large effect size
+modelsa <- read.csv("bws-interpolation-results/bws-interpolation-results-large.csv") 
 #turns results into dataframe
 modelsb <- as.data.frame(modelsa)
 #selects useful columns in dataframe (sample size and power)
-modelsc <- modelsb[,2:3] %>%
+bws_large <- modelsb[,2:3] %>%
         mutate_if(is.numeric, round, 2)
 #renames columns in dataframe
-colnames(modelsc) <- c("Sample Size", "Power")
+colnames(bws_large) <- c("Sample Size", "Power")
+
+#reads in csv for bws simulation results for small effect size
+modelsa <- read.csv("bws-interpolation-results/bws-interpolation-results-small.csv") 
+#turns results into dataframe
+modelsb <- as.data.frame(modelsa)
+#selects useful columns in dataframe (sample size and power)
+bws_small <- modelsb[,2:3] %>%
+        mutate_if(is.numeric, round, 2)
+#renames columns in dataframe
+colnames(bws_small) <- c("Sample Size", "Power")
+
+#reads in csv for bkmr interpolation results for large effect size
+models <- read.csv("bkmr-interpolation-results/bkmr-interpolation-results-large.csv")
+#turns results into dataframe
+models2 <- as.data.frame(models)
+#selects useful columns in dataframe (variable, sample size, power)
+bkmr_large <- models2[,2:4] %>%
+        mutate_if(is.numeric, round, 2)
+#renames columns in dataframe
+colnames(bkmr_large) <- c("Chemical","Sample Size","Power")
+
+#reads in csv for bkmr interpolation results for small effect size
+models <- read.csv("bkmr-interpolation-results/bkmr-interpolation-results-small.csv")
+#turns results into dataframe
+models2 <- as.data.frame(models)
+#selects useful columns in dataframe (variable, sample size, power)
+bkmr_small <- models2[,2:4] %>%
+        mutate_if(is.numeric, round, 2)
+#renames columns in dataframe
+colnames(bkmr_small) <- c("Chemical","Sample Size","Power")
 
 server <- function(input, output){
-        #if function uses models3 (glm simulation results) if glm is selected in selectInput
-        #uses modelsc (bws simulation results) if bws is selected in selectInput
-        output$table <- renderDataTable(if(input$sct == "GLM"){
+        #displays results of interpolation on simulation results depending on 
+        #model and effect level that user selects 
+        output$table <- renderDataTable(if(input$sct == "GLM" && input$effectsize == "Large"){
                 #create datatable for glm, highlight cell yellow if power is > 0.8
-                datatable(models3[(models3[,"Sample Size"] == input$num),],
+                datatable(glm_large[(glm_large[,"Sample Size"] == input$num),],
                           rownames = FALSE) %>% 
                         formatStyle('Power', backgroundColor = styleInterval(c(0.8,1.0), c("white","yellow","white")))
                 #create datatable for bws, highlight cell yellow if power is > 0.8
-        }else{datatable(modelsc[(modelsc[,"Sample Size"] == input$num),],
+        }else if (input$sct == "GLM" && input$effectsize == "Small") {
+                #create datatable for glm, highlight cell yellow if power is > 0.8
+                datatable(glm_small[(glm_small[,"Sample Size"] == input$num),],
+                          rownames = FALSE) %>% 
+                        formatStyle('Power', backgroundColor = styleInterval(c(0.8,1.0), c("white","yellow","white")))
+        } else if (input$sct == "BWS" && input$effectsize == "Large") {
+                datatable(bws_large[(bws_large[,"Sample Size"] == input$num),],
+                        rownames = FALSE) %>% 
+                        formatStyle('Power', backgroundColor = styleInterval(c(0.8,1.0), c("white","yellow","white")))}
+        else if (input$sct == "BWS" && input$effectsize == "Small") {
+                datatable(bws_small[(bws_small[,"Sample Size"] == input$num),],
+                          rownames = FALSE) %>% 
+                        formatStyle('Power', backgroundColor = styleInterval(c(0.8,1.0), c("white","yellow","white")))}
+        else if (input$sct == "BKMR" && input$effectsize == "Large") {
+                datatable(bkmr_large[(bkmr_large[,"Sample Size"] == input$num),],
+                          rownames = FALSE) %>% 
+                        formatStyle('Power', backgroundColor = styleInterval(c(0.8,1.0), c("white","yellow","white")))}
+        else {datatable(bkmr_small[(bkmr_small[,"Sample Size"] == input$num),],
                         rownames = FALSE) %>% 
                         formatStyle('Power', backgroundColor = styleInterval(c(0.8,1.0), c("white","yellow","white")))});
+        
         #uses nhanes2 to plot correlation matrix
         output$plot <- renderPlot(nhanes2 %>%
                                           cor() %>%
